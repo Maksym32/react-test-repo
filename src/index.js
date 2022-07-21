8f0bd03b9ec200b094ef59b6bc9aa701deaca552
Maksym32 <bednogo1@gmail.com>
2022-07-21 13:45:53 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+// // import './index.css';
+// import App from './App';
+// // import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+const Product = props => (
+  <div>
+    <img
+      src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+      alt="Tacos With Lime"
+      width="640"
+    />
+    <h2>Tacos With Lime</h2>
+    <p>Price: 10.99$</p>
+    <button type="button">Add to cart</button>
+  </div>
+);
+
+// В разметке компонент записывается как JSX-тег
+ReactDOM.createRoot(document.getElementById("root")).render(<Product />);
